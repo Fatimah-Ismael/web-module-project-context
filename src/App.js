ad79c6e82bb83faa7d4a5b357a998a59ae64658a
Fatimah Ismael <fatimahismael@yahoo.com>
2023-01-31 10:28:10 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import CartContext from './contexts/CartContext';
+import ProductContext from './contexts/ProductContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -12,21 +15,28 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		console.log(item)
 		// add the given item to the cart
+		setCart([ ...cart, item]);
+
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value ={cart}>
+			<ProductContext.Provider value={{products, addItem}}>
+			<Navigation  />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
+			</ProductContext.Provider>
+			</CartContext.Provider>
 		</div>
 	);
 }
